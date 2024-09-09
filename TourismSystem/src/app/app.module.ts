2814ee68451f31fbbbcfdfe92cebdb60e4dd5c9a
erick7amorim <erickamorimcarv@gmail.com>
2024-09-09 11:52:40 -0300

    feat: creating client signup page

--- TourismSystem/src/app/app.module.ts
+++ TourismSystem/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
+import { SignupClientComponent } from './basic/components/signup-client/signup-client.component';
 
 registerLocaleData(en);
 
@@ -20,7 +21,8 @@ registerLocaleData(en);
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    SignupClientComponent
   ],
   imports: [
     BrowserModule,
