52921f05d3f99cb20e7d51e86c417e3a68e9b5fe
erick7amorim <erickamorimcarv@gmail.com>
2024-09-09 11:02:44 -0300

    feat: creating register client and company service methods

--- TourismSystem/src/app/basic/services/auth/auth.service.ts
+++ TourismSystem/src/app/basic/services/auth/auth.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const BASIC_URL = 'http://localhost:8080/';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+
+  registerClient(signupRequestDTO: any): Observable<any>{
+    return this.http.post(BASIC_URL + "clientes/registrar", signupRequestDTO);
+  }
+
+  registerCompany(signupRequestDTO: any): Observable<any>{
+    return this.http.post(BASIC_URL + "companhias/registrar", signupRequestDTO);
+  }
 }
