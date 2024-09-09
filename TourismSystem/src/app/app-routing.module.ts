b9d3226e72915e83527d75737047078e0750f276
erick7amorim <erickamorimcarv@gmail.com>
2024-09-09 20:24:20 -0300

    feat: creating login page and authemtication logic

--- TourismSystem/src/app/app-routing.module.ts
+++ TourismSystem/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupClientComponent } from './basic/components/signup-client/signup-client.component';
 import { SignupCompanyComponent } from './basic/components/signup-company/signup-company.component';
+import { LoginComponent } from './basic/components/login/login.component';
 
 const routes: Routes = [
   { path:  'registrar_cliente', component: SignupClientComponent },
   { path:  'registrar_companhia', component: SignupCompanyComponent },
+  { path:  'login', component: LoginComponent },
   { path: 'companhia', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) },
   { path: 'cliente', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) }
 ];
--- TourismSystem/src/app/basic/components/login/login.component.ts
+++ TourismSystem/src/app/basic/components/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth/auth.service';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +11,32 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  validateForm!: FormGroup;
+
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private notification: NzNotificationService,
+    private router: Router,
+    ) {}
+
+    ngOnInit() {
+      this.validateForm = this.fb.group({
+        username : [null, [Validators.required]],
+        password : [null, [Validators.required]],
+      })
+    }
+
+    submitForm() {
+      this.authService.login(this.validateForm.get(['username'])!.value, this.validateForm.get(['password'])!.value)
+      .subscribe(res =>{
+        console.log(res);
+      }, error => {
+        this.notification
+        .error(
+          'ERROR',
+          `BAD credentials`,
+          { nzDuration: 5000 }
+        )
+      })
+    }
 }
