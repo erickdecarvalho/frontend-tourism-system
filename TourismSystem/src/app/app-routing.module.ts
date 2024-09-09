a7f7d10aca2e3b48b11ce40033c5c29d31b67d65
erick7amorim <erickamorimcarv@gmail.com>
2024-09-09 14:27:43 -0300

    feat: creating company signup page

--- TourismSystem/src/app/app-routing.module.ts
+++ TourismSystem/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignupClientComponent } from './basic/components/signup-client/signup-client.component';
+import { SignupCompanyComponent } from './basic/components/signup-company/signup-company.component';
 
 const routes: Routes = [
   { path:  'registrar_cliente', component: SignupClientComponent },
+  { path:  'registrar_companhia', component: SignupCompanyComponent },
   { path: 'companhia', loadChildren: () => import('./company/company.module').then(m => m.CompanyModule) },
   { path: 'cliente', loadChildren: () => import('./client/client.module').then(m => m.ClientModule) }
 ];
