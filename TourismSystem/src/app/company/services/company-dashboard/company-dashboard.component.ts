c7f09b288ff8c651c95b0f7a0eeece660c2843f8
erick7amorim <erickamorimcarv@gmail.com>
2024-09-12 14:36:49 -0300

    feat: listing all reservations of clients

--- TourismSystem/src/app/company/services/company-dashboard/company-dashboard.component.ts
+++ TourismSystem/src/app/company/services/company-dashboard/company-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CompanyService } from '../company.service';
 
 @Component({
   selector: 'app-company-dashboard',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class CompanyDashboardComponent {
 
+  services: any;
+
+  constructor(private companyService: CompanyService,) {}
+
+  ngOnInit() {
+    this.getAllTourismServices();
+  }
+
+  getAllTourismServices() {
+    this.companyService.getAllTourismServices().subscribe(res => {
+      console.log(res);
+      this.services = res;
+    })
+  }
 }
